edec90f5e1134767f270bc4dcd6e10401fe5c443
prabhnith <prabhjot.nith@gmail.com>
2019-10-09 23:18:29 +0530

    dynamic row creation

--- src/ui.js
+++ src/ui.js
@@ -9,5 +9,12 @@ let ui = {
 
 	updateOutput: (res) => {
 		document.getElementById("output").innerHTML = JSON.stringify(res);
+		let tableBody = document.getElementById("tableBody");
+		tableBody.innerHTML += `<tr>
+									<td class="column1"></td>
+									<td class="column2"></td>
+									<td class="column3"></td>
+									<td class="column4"></td>
+								</tr>`;
 	},
 };
